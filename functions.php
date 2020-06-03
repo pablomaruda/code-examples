fc6787b46c8f58d87f28d078c74bd4c5a9f83c1f
Pablo Maruda <66372603+pablomaruda@users.noreply.github.com>
2020-06-03 17:21:46 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,15 +2,15 @@
 function remove(int $number) : array {
 	$tasks = readFromFile();
 	unset($tasks[$number - 1]);
-	$tasks = array_values($tasks);
+	$tasks = array_values($tasks); //Uporządkowanie indeksów po sunięciu zadania
 	return $tasks;
 }
 function moveUp(int $number) : array {
 	$tasks = readFromFile();
-	if($number > 1 && $number <= count($tasks)) {
-		$slice = array_slice($tasks, ($number - 2), 2);
-		$tasks[($number - 2)] = $slice[1];
-		$tasks[($number - 1)] = $slice[0];
+	if($number > 1 && $number <= count($tasks)) { //sprawdzenie czy numer zadania do przeniesienia zawiera się w istniejących i czy nie próbujemy przenieść wyżej pierwszego zadania
+		$slice = array_slice($tasks, ($number - 2), 2); //pobranie wycinka tablicy $tasks z zadaniami do zamiany
+		$tasks[($number - 2)] = $slice[1]; //przypisanie zadania na nową pozycję
+		$tasks[($number - 1)] = $slice[0]; //jw
 	} else {
 		echo 'Wrong task number'. PHP_EOL;
 	}
@@ -18,10 +18,10 @@ function moveUp(int $number) : array {
 }
 function moveDown(int $number) : array {
 	$tasks = readFromFile();
-	if($number > 0 && $number < count($tasks)) {
-		$slice = array_slice($tasks, ($number - 1), 2);
-		$tasks[$number] = $slice[0];
-		$tasks[$number - 1] = $slice[1];
+	if($number > 0 && $number < count($tasks)) { //sprawdzenie czy numer zadania do przeniesienia zawiera się w istniejących i czy nie próbujemy przenieść niżej ostatniego zadania
+		$slice = array_slice($tasks, ($number - 1), 2); //pobranie wycinka tablicy $tasks z zadaniami do zamiany
+		$tasks[$number] = $slice[0]; //przypisanie zadania na nową pozycję
+		$tasks[$number - 1] = $slice[1]; //jw
 	} else {
 		echo 'Wrong task number'. PHP_EOL;
 	}
